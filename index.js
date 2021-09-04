636424d8646f53fbefc43cd5f9b0fb760f001793
nagysandordominik <nagysandordominik@icloud.com>
2021-09-04 19:26:06 +0200

    added new menu options and transform effect on mobile view

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 
 btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
+    menu.classList.toggle("transform");
 });
