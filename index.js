eb0a769f548c2321c799859a7a1a922337652e1e
nagysandordominik <nagysandordominik@icloud.com>
2021-09-08 19:29:45 +0200

    typing effect created

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 
 btn.addEventListener("click", () => {
     menu.classList.toggle("transform");
 });
+
+const header = document.querySelector('#typing');
+
+let typed = new Typed(header, {
+    strings: [
+        'Full-stack Developer',
+        'Front-end',
+        'Problem-Solver',
+        'Creative Solutions',
+        'Enthusiastic Learner',
+        'Freelancer',
+    ],
+    loop: true,
+    typeSpeed: 110,
+});
+
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
